927c3ab75cbe7027c55425c7089850e69d63bcff
Moraytaprime <149844584+Moraytaprime@users.noreply.github.com>
2023-11-15 15:04:44 -0600

    Add files via upload

---
+++ areas code.py
@@ -0,0 +1,9 @@
+base = 10
+altura= 15
+# Multiplicar base por altura para obtener Area
+
+Areac= (base * altura)
+print("La base del rectangulo es", Areac)
+# Dividir el Area entre 2 para obtener la de un triangulo
+Areatri=(Areac/2)
+print("La base del triangulo es", Areatri)
